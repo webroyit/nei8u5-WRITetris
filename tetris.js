342adc72d25c154fed85b5b87441f8a14c16188f
royweb <xytank@gmail.com>
2019-12-14 12:36:01 -0500

    Add a function to update the position of the shape piece

--- tetris.js
+++ tetris.js
@@ -4,10 +4,6 @@ const context = canvas.getContext("2d");
 // make the shape bigger
 context.scale(20, 20);
 
-// styles the context
-context.fillStyle = "#e6f2ff";   // add color
-context.fillRect(0, 0, canvas.width, canvas.height);    // add sizes to make a rectangle shape
-
 // create the layout of the shapes
 const matrix = [
     [0, 0, 0],
@@ -17,6 +13,10 @@ const matrix = [
 
 // add the shape to the board
 function draw(){
+    // styles the context
+    context.fillStyle = "#e6f2ff";   // add color
+    context.fillRect(0, 0, canvas.width, canvas.height);    // add sizes to make a rectangle shape
+
     drawMatrix(player.matrix, player.position);
 }
 
@@ -34,9 +34,18 @@ function drawMatrix(matrix, offset){
     });
 }
 
+// update the position of the shape
+function update(){
+    draw();
+    
+    // update the dom
+    requestAnimationFrame(update);
+
+}
+
 const player = {
     position: {x: 5, y: 3},
     matrix: matrix
 }
 
-draw();
+update();
