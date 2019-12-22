22c692fc340cd128efe3ee255088e2462330e6d7
royweb <xytank@gmail.com>
2019-12-22 11:44:11 -0500

    Set up websocket

--- server/server.js
+++ server/server.js
@@ -1,3 +1,16 @@
 const WebSocketServer = require("ws").Server;
 
 const server = new WebSocketServer({ port: 7000 });
+
+// start the websocket
+server.on("connection", conn => {
+    console.log("Websocket Online");
+
+    conn.on("message", msg => {
+        console.log("Message received", msg);
+    })
+
+    conn.on("close", () => {
+        console.log("Connection Closed");
+    })
+});
