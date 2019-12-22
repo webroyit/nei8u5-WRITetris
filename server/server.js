45ad615b36cf45c51200406cf17b8a3f59737fd2
royweb <xytank@gmail.com>
2019-12-22 14:34:01 -0500

    Add a class for session

--- server/server.js
+++ server/server.js
@@ -2,12 +2,26 @@ const WebSocketServer = require("ws").Server;
 
 const server = new WebSocketServer({ port: 7000 });
 
+const sessions = new Map;
+
+class Session{
+    constructor(id){
+        this.id = id;
+    }
+}
+
 // start the websocket
 server.on("connection", conn => {
     console.log("Websocket Online");
 
     conn.on("message", msg => {
         console.log("Message received", msg);
+
+        if(msg === "create-session"){
+            const session = new Session("foobar");
+            sessions.set(session.id, session);
+            console.log(sessions);
+        }
     })
 
     conn.on("close", () => {
