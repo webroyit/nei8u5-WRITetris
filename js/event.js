23164adf4f156e0efd9405c80e9619e5ce774db7
royweb <xytank@gmail.com>
2019-12-24 16:05:18 -0500

    Add listen and emit functions to event.js

--- js/event.js
+++ js/event.js
@@ -2,4 +2,20 @@ class Events{
     constructor(){
         this._listeners = new Set;
     }
+
+    listen(name, callback){
+        this._listeners.add({
+            name,
+            callback
+        })
+    }
+
+    // ...data is same as data[0], data[1], data[2]
+    emit(name, ...data){
+        this._listeners.forEach(listener => {
+            if(listener.name === name){
+                listener.callback(...data);
+            }
+        })
+    }
 }
